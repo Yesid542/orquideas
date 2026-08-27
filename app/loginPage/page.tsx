dfa2d24a0658117acd6bd1089f07070e3f975fa5
yesid <yesidbarreto24@gmail.com>
2026-08-27 18:20:38 -0500

    fin pruebas 3

--- app/loginPage/page.tsx
+++ app/loginPage/page.tsx
@@ -1,5 +1,11 @@
+// app/loginPage/page.tsx
+import React, { Suspense } from "react";
 import LoginPageComponent from "./LoginPageComponent";
 
 export default function Page() {
-  return <LoginPageComponent />;
+  return (
+    <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Cargando...</div>}>
+      <LoginPageComponent />
+    </Suspense>
+  );
 }
